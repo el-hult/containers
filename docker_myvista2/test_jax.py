35353714db766fea3be5cd6f6c28435b1e8084ca
Ludvig Hult <ludvig.hult@gmail.com>
2025-10-30 09:22:31 +0100

    remove unused deprecated docker images

--- docker_myvista2/test_jax.py
+++
@@ -1,3 +0,0 @@
-print(f"Running test file {__file__}")
-import jax
-print(jax.devices())
--- docker_myvista2/test_transformers.py
+++
@@ -1,4 +0,0 @@
-from transformers import pipeline
-classifier = pipeline("sentiment-analysis")
-ans =classifier("We are very happy to show you the 🤗 Transformers library.")
-print(ans)
